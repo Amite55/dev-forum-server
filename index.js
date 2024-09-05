cd4fbfc2fcff0a0fc7521295ef8cf4920ea86ddd
Amite55 <amite403@gmail.com>
2024-09-05 20:49:31 +0600

    added jwt with local storage

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
+const app = express();
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
@@ -10,30 +9,27 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middleware =====
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
-  credentials: true,
-  optionSuccessStatus: 200,
-}
+  origin: ['http://localhost:5173', 'https://dev-forum-1d988.web.app', 'https://dev-forum-1d988.firebaseapp.com', 'https://dev-forum-platform-server-side.vercel.app']
+};
 app.use(cors(corsOptions))
 app.use(express.json());
-app.use(cookieParser());
 
 // Verify Token Middleware
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token
-  console.log(token, 'token')
-  if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
+const verifyToken = (req, res, next) => {
+  // console.log('inside verify token', req.headers.authorization);
+  if(!req.headers.authorization){
+    return res.status(401).send({message: 'unauthorized access'})
   }
+  const token = req.headers.authorization.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
     }
-    req.user = decoded
-    next()
+    req.decoded = decoded;
+    next();
   })
 }
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g2fbusk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -49,7 +45,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const postedCollection = client.db('devForum').collection('postedData');
     const usersCollection = client.db('devForum').collection('users');
@@ -59,40 +55,32 @@ async function run() {
     const reportsCollection = client.db('devForum').collection('report');
     const feedbackCollection = client.db('devForum').collection('feedback');
 
-    // verify admin middleware =======
+
     const verifyAdmin = async (req, res, next) => {
-      const user = req.user;
-      const query = { email: user?.email };
-      const result = await usersCollection.findOne(query);
-      if (!result || result?.role !== 'admin') return res.status(401).send({ message: 'Unauthorized Access!!' });
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'});
+      }
       next();
     }
 
     // auth related api ======== jwt==========
     app.post('/jwt', async (req, res) => {
-      const user = req.body
+      const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '365d',
-      })
-      res
-        .cookie('token', token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+           expiresIn: '365d' 
         })
-        .send({ success: true })
+        console.log(token, 'this is token');
+      res.send({token})
     })
 
     // Logout =========== token ======== jwt======
     app.get('/logout', async (req, res) => {
       try {
-        res
-          .clearCookie('token', {
-            maxAge: 0,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-          })
-          .send({ success: true })
+        res.send({ success: true })
         console.log('Logout successful')
       } catch (err) {
         res.status(500).send(err)
@@ -123,9 +111,7 @@ async function run() {
       // check if user is already exists in Database 
       const isExist = await usersCollection.findOne(query);
       if (isExist) return res.send(isExist);
-
-      const options = { upsert: true }
-        ;
+      const options = { upsert: true };
       const updateDoc = {
         $set: {
           ...user,
@@ -207,7 +193,6 @@ async function run() {
       const page = parseInt(req.query.page) - 1;
        const size = parseInt(req.query.size);
        const sort = req.query.sort;
-       console.log(sort, 'hey ami sort');
       let filter = {};
       if(search){
         console.log(search);
@@ -324,7 +309,7 @@ async function run() {
     })
 
     // delete report activity page ======
-    app.delete('/report/deleted/:id', async (req, res) => {
+    app.delete('/report/deleted/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await reportsCollection.deleteOne(query);
@@ -346,8 +331,8 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB! devForum ");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB! devForum ");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
